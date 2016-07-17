dc2965cdb53ff2e6d5d23e7b32994f267cf12fa4
Alyson Takai <shtakai@gmail.com>
2016-07-17 14:45:18 -0700

    modified

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// set static directory
+app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, './bower_components')));
+
+
+let server = app.listen(port, () =>{
+  console.log(`server listening on TCP/${port}`);
+})
+
+
+
