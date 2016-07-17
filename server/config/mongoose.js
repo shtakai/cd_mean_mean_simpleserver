a06446500689f02d9a2232a5e16c400b9839ccd0
Alyson Takai <shtakai@gmail.com>
2016-07-17 16:00:01 -0700

    events on mongoose

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -9,6 +9,36 @@ mongoose.connect(dbURL)
 
 const modelsPath = path.join( __dirname, './../models' )
 
+// Connection event
+
+// successfully connected
+mongoose.connection.on( 'connected', () => {
+  console.log(`Mongoose default connection opened to ${dbURL}`)
+} )
+
+// connection throws an error
+mongoose.connection.on( 'error', (err) => {
+  console.error(`Mongoose default connection error: ${err}`)
+} )
+
+// connection is disconnected
+mongoose.connection.on( 'disconnected', () => {
+  console.log(`Mongoose default connection disconnected`)
+} )
+
+/*
+ * if the node process ends, close the Mongoose connection
+ */
+process.on( 'SIGINT', () => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose default connection disconnected through app termination`)
+  } )
+} )
+
+/*
+ * read all of the files in the models dir
+ *  and dheck if it is a javascript file before requiring it
+ */
 fs.readdirSync(modelsPath).forEach( (file) => {
   if( reg.test( file )){
     require(`${modelsPath}/${file}`)
