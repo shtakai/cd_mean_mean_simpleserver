16f9f6e6565933a5caa5aa8d1a3601159b76c87c
Alyson Takai <shtakai@gmail.com>
2016-07-17 15:32:31 -0700

    removed semicolon

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-require('dotenv').config();
-const port = process.env.PORT || 8000;
+require('dotenv').config()
+const port = process.env.PORT || 8000
 
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 // set static directory
-app.use(express.static(path.join(__dirname, './client')));
-app.use(express.static(path.join(__dirname, './bower_components')));
+app.use(express.static(path.join(__dirname, './client')))
+app.use(express.static(path.join(__dirname, './bower_components')))
 
 // load mongoose/model
-require('./server/config/mongoose.js');
+require('./server/config/mongoose.js')
 
 // route_setter
-const routes_setter = require('./server/config/routes.js');
-routes_setter(app);
+require('./server/config/routes.js')(app)
 
 let server = app.listen(port, () =>{
-  console.log(`server listening on TCP/${port}`);
+  console.log(`server listening on TCP/${port}`)
 })
 
 
