a4bf8ec2bc307e95a2a635b2844a7c8a6060b2c4
Alyson Takai <shtakai@gmail.com>
2016-07-17 15:51:32 -0700

    use regexp on loading js models

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-const dbURL = process.env.DB_URL;
-const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
+require('dotenv').config()
+const dbURL = process.env.DB_URL
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
+const reg = new RegExp('.js$', 'i')
 
-mongoose.connect(dbURL);
+mongoose.connect(dbURL)
 
-const modelsPath = path.join( __dirname, './../models' );
+const modelsPath = path.join( __dirname, './../models' )
 
 fs.readdirSync(modelsPath).forEach( (file) => {
-  if(file.indexOf('.js') >= 0){
-    require(`${modelsPath}/${file}`);
+  if( reg.test( file )){
+    require(`${modelsPath}/${file}`)
   }
-} );
+} )
