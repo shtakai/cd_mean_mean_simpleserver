e37f38ed90efdcf22c31568a3c089942c3014fb7
Alyson Takai <shtakai@gmail.com>
2016-07-17 15:39:09 -0700

    add routes for friends

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+console.log(' routes')
 
+const mongoose = require('mongoose')
 
-module.exports = function(app){
-  console.log('future routes')
-  //app.get('/test', (req, res) => {
-    //res.json({
-      //test: 'this is test'
-    //})
-  //})
+module.exports = (app) => {
+  app.get('/friends'        , friends.index)
+  app.get('/friends/:id'    , friends.show)
+  app.post('/friends'       , friends.create)
+  app.put('/friends/:id'    , friends.update)
+  app.delete('/friends/:id' , friends.delete)
 }
