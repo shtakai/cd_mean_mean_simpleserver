1d3b0100e4eab2f9ed98596a0711940e8b56f1c3
Alyson Takai <shtakai@gmail.com>
2016-07-18 10:37:19 -0700

    modified editController

--- client/assets/controllers/editController.js
+++ client/assets/controllers/editController.js
@@ -1,14 +1,26 @@
 app.controller(
   'editController',
-    function( $location, $routeParams ){
-      console.log('editController start');
-      console.log('routeParams', $routeParams)
-      console.log('location', $location);
-      this.edittext = 'edit'
+  function( friendsFactory, $location, $routeParams ){
+    console.log('editController start');
+    console.log('routeParams', $routeParams)
+    console.log('location', $location);
+    this.edittext = 'edit'
+    /**
+     * GET A FRIEND FROM THE FACTORY, This is a one time thing when
+     * we load this partial, so we didn't set a variable so we could reuse it,
+     * rather, just ran the friendsFactory method directly.
+     */
 
-      this.update = () => {
-         console.log('editController update')
-         console.log(`first name:${this.firstName}  last name:${this.lastName}`)
-      }
-    }
+    friendsFactory.getFriend((returnedData) => {
+      this.friend = returnedData
+      console.log(this.friend)
+    })
+
+    /**
+     * OUR $scope.update function goes here <-- $scope because we need to
+     * access this method with ng-submit or ng-click (from the form in the
+     * previous assignment).  Want to see all of the friends when we get
+     * back including the updated on??  See Index in the previous controller.
+     */
+  }
 )
