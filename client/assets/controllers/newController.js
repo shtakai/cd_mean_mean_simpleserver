0b2476efa7698df69652bb376d4a4449ca96e816
Alyson Takai <shtakai@gmail.com>
2016-07-18 10:34:14 -0700

    modified newController

--- client/assets/controllers/newController.js
+++ client/assets/controllers/newController.js
@@ -1,6 +1,7 @@
 app.controller(
   'newController',
-    function( $location, $routeParams ){
+    function( friendsFactory, $location, $routeParams ){
+
       console.log('newController start');
       console.log('routeParams', $routeParams)
       console.log('location', $location);
@@ -11,5 +12,25 @@ app.controller(
          console.log(`first name:${this.firstName}  last name:${this.lastName}`)
       }
 
+      /**
+       *   THIS INDEX METHOD ACCESSES THE FRIENDS FACTORY AND RUNS
+       *   THE FRIENDS INDEX.
+       *   WE MIGHT RE USE INDEX A FEW TIMES,
+       *   SO TO MINIMIZE REPETITION WE SET IT AS A VARIABLE.
+       */
+
+      let index = () => {
+        friendsFactory.index((returnedData) => {
+          this.friends = returnedData
+          console.log(this.friends)
+        })
+      }
+      index()
+      /**
+       *  OUR $scope.create function goes here <--
+       *  $scope because we need to access this method with ng-submit or
+       *  ng-click (from the form in the previous assignment).
+       *  Want to all of the friends when we get back?  We can re-run index.
+       */
     }
 )
