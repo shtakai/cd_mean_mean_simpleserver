19795704748125733deb06570283fd83f6d8ceee
Alyson Takai <shtakai@gmail.com>
2016-07-18 16:23:18 -0700

    modified route

--- client/assets/app.js
+++ client/assets/app.js
@@ -2,6 +2,11 @@ const app = angular.module('app', ['ngRoute'])
 
 app.config(($routeProvider) => {
   $routeProvider
+    .when('/', {
+      templateUrl: '../partials/index.html',
+      controller: 'newController',
+      controllerAs: 'c',
+    })
     .when('/new', {
       templateUrl: '../partials/new.html',
       controller: 'newController',
@@ -12,11 +17,24 @@ app.config(($routeProvider) => {
       controller: 'showController',
       controllerAs: 'c',
     })
+    .when('/delete/:id', {
+      templateUrl: '../partials/delete.html',
+      controller: 'deleteController',
+      controllerAs: 'c',
+    })
     .when('/edit/:id', {
       templateUrl: '../partials/edit.html',
       controller: 'editController',
       controllerAs: 'c',
     })
+    .otherwise({
+      redirectTo: '/'
+    })
+    //.when('/update/d', {
+      //templateUrl: '../partials/update.html',
+      //controller: 'updateController',
+      //controllerAs: 'c',
+    //})
 });
 
 /**
