d3d15c4018bdce496b6f98aaa5c7e8032924f4a9
Alyson Takai <shtakai@gmail.com>
2016-07-17 19:34:04 -0700

    create empty file

---
+++ client/assets/app.js
@@ -0,0 +1,6 @@
+const app = angular.module('app', ['ngRoute'])
+
+app.config( ($routeProvider) => {
+  console.log('app-config')
+// Routes to load your new and edit pages with new and edit controllers attached to them!
+} )
