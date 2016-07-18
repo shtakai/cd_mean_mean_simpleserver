7e37ee7f893e199b9bd2d99db74c2eead9131c02
Alyson Takai <shtakai@gmail.com>
2016-07-17 19:55:22 -0700

    add routes/controller template

--- client/assets/app.js
+++ client/assets/app.js
@@ -2,5 +2,34 @@ const app = angular.module('app', ['ngRoute'])
 
 app.config( ($routeProvider) => {
   console.log('app-config')
-// Routes to load your new and edit pages with new and edit controllers attached to them!
+  // Routes to load your new and edit pages with new and edit
+  //  controllers attached to them!
+  $routeProvider
+    .when('/new', {
+      templateUrl: '../partials/new.html',
+      controller: 'FriendsController',
+      controllerAs: 'FC',
+    })
+    .when('/edit/:id', {
+      //  /edit/:id ?
+      templateUrl: '../partials/edit.html',
+      controller: 'FriendsController',
+      controllerAs: 'FC',
+    })
+    .otherwise({
+      redirectTo: '/',
+    })
+
 } )
+
+
+/**
+ * Now Just put controller in it.
+ */
+
+app.controller(
+  'FriendsController',
+  (FriendsController, $location, $routeParams) => {
+     console.log('FriendsController start')
+  }
+)
